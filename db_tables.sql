1157653202dbb46fc90253b96b0f515e3d25bf82
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2023-05-29 11:02:38 -0700

    Update db_tables.sql

--- db_tables.sql
+++ db_tables.sql
@@ -31,4 +31,62 @@ CREATE TABLE record_label_artists {
 artist_id int REFERENCES artist(id),
 record_label_id int REFERENCES record_label(id),
 PRIMARY KEY(artist_id, record_label_id)
+};
+
+CREATE TABLE album {
+id int PRIMARY KEY,
+name varchar(50),
+release_date timestamp,
+length_seconds int
+};
+
+CREATE TABLE track {
+id int PRIMARY KEY,
+name varchar(50),
+length_seconds int,
+release_date timestamp,
+people_id int
+};
+
+CREATE TABLE track_artists {
+track_id int REFERENCES track(id),
+artist_id int REFERENCES artist(id),
+PRIMARY KEY(track_id, song_id)
+};
+
+CREATE TABLE album_artists {
+album_id int REFERENCES album(id),
+artist_id int REFERENCES artist(id),
+PRIMARY KEY(album_id, artist_id)
+}
+
+CREATE TABLE album_tracks {
+album_id int REFERENCES album(id),
+track_id int REFERENCES track(id)
+}
+
+CREATE TABLE playlist {
+id int PRIMARY KEY,
+name varchar(100),
+song_id int
+};
+
+CREATE TABLE playlist_tracks {
+track_id int REFERENCES track(id),
+playlist_id int REFERENCES playlist(id),
+PRIMARY KEY(track_id, playlist_id)
+}
+
+CREATE TABLE role {
+id int PRIMARY KEY,
+role varchar(50),
+description varchar(254)
+};
+
+CREATE TABLE role_information {
+role_id int REFERENCES role(id),
+artist_id int REFERENCES artist(id),
+start timestamp,
+end timestamp,
+PRIMARY KEY(role_id, artist_id)
 }
