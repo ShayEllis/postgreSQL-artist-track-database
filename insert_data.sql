7cc71f27101e0ec21de4a682f55166e0f2720ea7
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2023-06-05 21:01:01 -0700

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO record_label
+values 
+	(1, 'Strange Music', 1999, 1, 'Lee''s Summit', 'MO', 64086, 'https://www.strangemusicinc.com/', 'https://www.strangemusicinc.com/img'), 
+	(2, 'Shady Records', 1999, 1, 'Santa Monica', 'CA', 90404, 'https://www.shadyrecords.com/', 'https://www.shadyrecords.com/img'), 
+	(3, 'Def Jam Recordings', 1984, 1, 'New York', 'New York', 10019, 'https://www.defjam.com/#/', 'https://www.defjam.com/#/img')
