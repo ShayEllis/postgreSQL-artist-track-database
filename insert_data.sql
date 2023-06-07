aab7d2e1e81aa0f2d87926ff5c1863ef856c90ea
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2023-06-07 08:04:09 -0700

    Update insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -1,5 +1,25 @@
+INSERT INTO country
+VALUES
+	(86, 'China', 'CN'),
+	(91, 'India', 'IN'),
+	(1, 'United States', 'US'),
+	(62, 'Indonesia', 'ID');
+
 INSERT INTO record_label
-values 
-	(1, 'Strange Music', 1999, 1, 'Lee''s Summit', 'MO', 64086, 'https://www.strangemusicinc.com/', 'https://www.strangemusicinc.com/img'), 
-	(2, 'Shady Records', 1999, 1, 'Santa Monica', 'CA', 90404, 'https://www.shadyrecords.com/', 'https://www.shadyrecords.com/img'), 
-	(3, 'Def Jam Recordings', 1984, 1, 'New York', 'New York', 10019, 'https://www.defjam.com/#/', 'https://www.defjam.com/#/img')
+VALUES 
+	(1, 'Strange Music', make_date(1999, 1, 1), 1, E'Lee\'s Summit', 'MO', 64086, 'https://www.strangemusicinc.com/', 'https://www.strangemusicinc.com/img'), 
+	(2, 'Shady Records', make_date(1999, 1, 1), 1, 'Santa Monica', 'CA', 90404, 'https://www.shadyrecords.com/', 'https://www.shadyrecords.com/img'), 
+	(3, 'Def Jam Recordings', Make_date(1984, 1, 1), 1, 'New York', 'New York', 10019, 'https://www.defjam.com/#/', 'https://www.defjam.com/#/img');
+	
+INSERT INTO artist
+VALUES
+	('Aaron', 'Yates', 'Tech N9ne', 1, make_date(1971, 11, 8)),
+	(),
+	(),
+	();
+	
+INSERT INTO album
+VALUES
+	(),
+	(),
+	();
