104a62307219b4946344280e2a5077375ee89bef
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2023-06-07 18:41:14 -0700

    Update table_alterations.sql

--- table_alterations.sql
+++ table_alterations.sql
@@ -1,10 +1,13 @@
 ALTER TABLE artist 
 ALTER COLUMN dob TYPE int 
-USING extract(epock FROM dob)
+USING extract(epock FROM dob);
 
 ALTER TABLE artist
 ALTER COlUMN dob TYPE timestamp
-USING to_timestamp(dob)
+USING to_timestamp(dob);
 
 ALTER TABLE track
-DROP COLUMN people_id
+DROP COLUMN people_id;
+
+ALTER TABLE playlist
+DROP COLUMN song_id;
