6efba073bfc0d3685788754d34deeaefd270c54c
ShayEllis <58398990+ShayEllis@users.noreply.github.com>
2023-06-07 18:03:06 -0700

    Update insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -13,13 +13,18 @@ VALUES
 	
 INSERT INTO artist
 VALUES
-	('Aaron', 'Yates', 'Tech N9ne', 1, make_date(1971, 11, 8)),
-	(),
-	(),
-	();
+	(1, 'Aaron', 'Yates', 'Tech N9ne', 1, make_date(1971, 11, 8), 1991, NULL),
+	(2, 'Michael', 'Atha', 'Yelawolf', 1, make_date(1997, 12, 30), 2005, NULL),
+	(3, 'Morgan', 'Wallen', 'Morgan Wallen', 1, make_date(1993, 5, 13), 2014, NULL),
+	(4, 'Artis', 'Ivey', 'Coolio', 1, make_date(1963, 8, 1), 1987, 2022);
 	
 INSERT INTO album
 VALUES
-	(),
-	(),
-	();
+	(1, 'Tech Album 1', make_date(2002, 1, 23), 7380),
+	(2, 'Tech Album 2', make_date(2021, 5, 11), 8132),
+	(2, 'Yelawolf Album 1', make_date(2012, 8, 17), 6623),
+	(4, 'Yelawolf Album 2', make_date(2018, 11, 2), 7746),
+	(5, 'Morgan Wallen Album 1', make_date(2023, 12, 1), 5987),
+	(6, 'Morgan Wallen Album 2', make_date(2022, 3, 12), 6749),
+	(7, 'Coolio Album 1', make_date(2001, 3, 23), 4544),
+	(8, 'Coolio Album 2', make_date(1996, 6, 30), 5897);
